de156ded51098eeb597ac1497e959bc91c669ada
Marogatake <69722304+Marogatake@users.noreply.github.com>
2021-03-04 16:46:26 +0900

    Add files via upload

--- script/nav.js
+++ script/nav.js
@@ -1,6 +1,7 @@
 $(function () {
   var winWidth = $(window).width(),
   navBox = $('#header_menu'),
+  productText = $('#product_text'),
   storyPosition = $('#fadein_title_story').offset().top,
   scenePosition = $('#fadein_title_scene').offset().top,
   productPosition = $('#fadein_title_product').offset().top,
@@ -11,22 +12,38 @@ $(function () {
   $(window).on('resize',function(){
     clearTimeout( resizeNav );
     resizeNav = setTimeout(function() {
-      winWidth = $(window).width();
+      winWidth = $(window).width(),
+      storyPosition = $('#fadein_title_story').offset().top,
+      scenePosition = $('#fadein_title_scene').offset().top,
+      productPosition = $('#fadein_title_product').offset().top,
+      assesmentPosition = $('#fadein_title_assessment').offset().top,
+      faqPosition = $('#fadein_title_faq').offset().top;
     });
   });
 
+  productText.on('change',function(){
+    winWidth = $(window).width(),
+    storyPosition = $('#fadein_title_story').offset().top,
+    scenePosition = $('#fadein_title_scene').offset().top,
+    productPosition = $('#fadein_title_product').offset().top,
+    assesmentPosition = $('#fadein_title_assessment').offset().top,
+    faqPosition = $('#fadein_title_faq').offset().top;
+  });
+
   $('.nav-btn').on('click',function(){
-    if( $(this).hasClass('active') ){
-      $(this).removeClass('active');
-      $('.header__right').addClass('close').removeClass('open');
-      $("body").css('overflow','auto');
-      $('header').css('opacity','0.8');
-    }
-    else {
-      $(this).addClass('active');
-      $('.header__right').addClass('open').removeClass('close');
-      $("body").css('overflow','hidden');
-      $('header').css('opacity','0.9');
+    if (winWidth<=800) {
+      if( $(this).hasClass('active') ){
+        $(this).removeClass('active');
+        $('.header__right').addClass('close').removeClass('open');
+        $("body").css('overflow','auto');
+        $('header').css('opacity','0.8');
+      }
+      else {
+        $(this).addClass('active');
+        $('.header__right').addClass('open').removeClass('close');
+        $("body").css('overflow','hidden');
+        $('header').css('opacity','0.9');
+      }
     }
   });
 
